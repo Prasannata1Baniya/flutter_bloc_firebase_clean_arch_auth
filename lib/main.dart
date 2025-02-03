380010c5025d0beb71909f6fc2b558bedd42fe07
Prasannata Baniya <prasannata250baniya@gmail.com>
2025-02-03 17:12:01 +0545

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_firebase/features/auth/data/repos-impl/auth_repo_impl.dart';
-import 'package:flutter_bloc_firebase/features/auth/presentation/cubit/auth_cubit.dart';
+import 'features/auth/data/repos-impl/auth_repo_impl.dart';
+import 'features/auth/presentation/cubit/auth_cubit.dart';
+import 'features/auth/presentation/cubit/auth_state.dart';
+import 'features/auth/presentation/pages/auth_page.dart';
+import 'features/auth/presentation/pages/home_page.dart';
 import 'firebase_options.dart';
 
 
@@ -30,9 +33,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-     // home: const MyHomePage(),
-    );
+    return BlocConsumer<AuthCubit,AuthState>(
+        builder: (context,state){
+      if(state is LoadingState){
+        return const Center(child: CircularProgressIndicator());
+      }
+      else if(state is AuthenticatedState){
+        return const HomePage();
+      }
+      else if(state is UnAuthenticatedState){
+        return const AuthPage(isLogin: true);
+      }
+    else{
+      return const Center(child:Text("Failed"));
+      }
+    },
+        listener: (context,state){
+          if(state is AuthErrorState){
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(state.error)));
+          }
+        });
   }
 }
